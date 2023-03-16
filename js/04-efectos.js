cc76e6e72805ee662ba49c238ad614c0ed33c516
Facundo <facurtprn@gmail.com>
2023-03-16 12:15:02 -0300

    commit

--- js/04-efectos.js
+++ js/04-efectos.js
@@ -26,10 +26,6 @@ $(document).ready(function(){
     });
 
 
-    var jq = document.createElement('script');
-jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
-// ... give time for script to load, then type.
-jQuery.noConflict()
+ 
 
 });
