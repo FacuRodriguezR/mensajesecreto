a2fbf8fecd9f3e198b14a8fa2a371fd84a7dab0a
Facundo <facurtprn@gmail.com>
2023-03-16 12:01:31 -0300

    hola

--- js/04-efectos.js
+++ js/04-efectos.js
@@ -26,4 +26,10 @@ $(document).ready(function(){
     });
 
 
+    var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+// ... give time for script to load, then type.
+jQuery.noConflict()
+
 });
